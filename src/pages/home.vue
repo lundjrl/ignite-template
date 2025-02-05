78f1e2c9d7a1f15fb6fecc36bf1200df1356438b
jam <jrlbidamin@gmail.com>
2025-02-05 11:47:27 -0500

    got tailwind to work with primevue styled

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,10 +2,13 @@
   <h1>Home</h1>
   <Button label="Submit" />
 
+  <!-- Example TW style -->
+  <div class="h-4 w-4 bg-amber-600"></div>
+
   <Card>
     <template #title>Simple Card</template>
     <template #content>
-      <p>
+      <p class="font-bold">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed
         consequuntur error repudiandae numquam deserunt quisquam repellat libero
         asperiores earum nam nobis, culpa ratione quam perferendis esse,
