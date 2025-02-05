b9a8a4b8c650de50174c68a35e3f55675588aba7
wmo <jrlbidamin@gmail.com>
2025-02-04 21:41:05 -0500

    Added basic routing and store

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { router } from './routing/routes'
+import storeConfig from './stores/config'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(storeConfig)
+    .use(router)
+    .mount('#app') 
